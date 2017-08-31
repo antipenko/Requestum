688590f1b243b30f6b08cd9a74fed92709f2ed2c
antipenko <sweetglory.victory@gmail.com>
2017-08-31 15:33:49 +0300

    delete Unnecessary js files

--- dist/index.php
+++ dist/index.php
@@ -159,8 +159,6 @@
     </footer>
 
 	<script src="js/jquery.min.js"></script>
-	<script src="js/what-input.min.js"></script>
-	<script src="js/foundation.min.js"></script>
 	<script src="js/app.js"></script>
 <!--	<script src="js/all.min.js"></script>-->
 </body>
